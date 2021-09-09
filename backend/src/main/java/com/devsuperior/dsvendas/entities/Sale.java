1ffcf1cbf72c27668230b6e5f895dd00dc67aebb
Vitor Hugo <f-winchester02@hotmail.com>
2021-09-09 11:25:40 -0300

    Domain model, database seed

---
+++ backend/src/main/java/com/devsuperior/dsvendas/entities/Sale.java
@@ -0,0 +1,30 @@
+package com.devsuperior.dsvendas.entities;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+@Table(name = "tb_sales")
+public class Sale {
+
+    @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Integer visited;
+    private Integer deals;
+    private Double amount;
+    private LocalDate date;
+
+    @ManyToOne
+    @JoinColumn(name = "seller_id")
+    private Seller seller;
+}
---
+++ backend/src/main/java/com/devsuperior/dsvendas/entities/Seller.java
@@ -0,0 +1,27 @@
+package com.devsuperior.dsvendas.entities;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+@Table(name = "tb_sellers")
+public class Seller {
+
+    @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    @OneToMany(mappedBy = "seller")
+    private List<Sale> sales = new ArrayList<>();
+}

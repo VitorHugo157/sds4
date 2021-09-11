549da1d9e6643168cf403b93f00bc921b5a6fe6e
Vitor Hugo <f-winchester02@hotmail.com>
2021-09-11 17:10:00 -0300

    Home, navigation

--- frontend/src/Routes.tsx
+++ frontend/src/Routes.tsx
@@ -1,6 +1,6 @@
-import Dashboard from 'pages/Dashboard';
-import Home from 'pages/Home';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Dashboard from "pages/Dashboard";
+import Home from "pages/Home";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 const Routes = () => {
   return (
@@ -9,13 +9,13 @@ const Routes = () => {
         <Route path="/" exact>
           <Home />
         </Route>
-        
+
         <Route path="/dashboard">
           <Dashboard />
         </Route>
       </Switch>
     </BrowserRouter>
   );
-}
+};
 
 export default Routes;
